093d0142b43f06d054c4ea9ae43058fc50365c57
tyagdit <tyagdit@gmail.com>
2020-04-15 00:39:44 +0530

    Reformatted

--- search.py
+++ search.py
@@ -1,8 +1,5 @@
 import qbittorrentapi
 import time
-from hurry.filesize import size
-
-pp = pprint.PrettyPrinter(indent=4)
 
 qbt_client = qbittorrentapi.Client(host='192.168.1.248:8080', username='admin', password='adminadmin')
 
@@ -11,7 +8,6 @@ try:
 except qbittorrentapi.LoginFailed as e:
     print(e)
 
-#def search(query):
 search_job = qbt_client.search.start(pattern='lucifer', category='all', plugins='all')
 
 while (search_job.status()[0].status != 'Stopped'):
@@ -21,6 +17,7 @@ search_result = qbt_client.search_results(id=search_job.id, limit = 3, offset = 
 
 for result in search_result.results:
     i = result.fileUrl.index('&')
-    print(f"Name: {result.fileName}\nMagnet: {result.fileUrl[:i]}\nSeeders: {result.nbSeeders}\nSize: {size(result.fileSize)}")
+    print(f"Name: {result.fileName}\nSeeders: {result.nbSeeders}\nSize: {result.fileSize}")
+    print(f"\nMagnet: {result.fileUrl[:i]}")
 
 search_job.delete()

2ad67ff691f163d116a5d59a6e0f8b8b5d6f59cb
tyagdit <tyagdit@gmail.com>
2020-04-15 23:13:30 +0530

    Add inline keyboard and magnet output

--- main.py
+++ main.py
@@ -6,68 +6,85 @@ import logging
 import qbittorrentapi
 import time
 
+class qbt_search():
+        
+    def __init__(self, search_term):
+
+        self.qbt_client = qbittorrentapi.Client(host='', username='', password='')
+
+        try:
+            self.qbt_client.auth_log_in()
+        except qbittorrentapi.LoginFailed as e:
+            print(e)
+        
+        self.search_job = self.qbt_client.search_start(pattern=search_term, category='all', plugins='all')
+        
+        while (self.search_job.status()[0].status != 'Stopped'):
+            time.sleep(.1)
+
+        self.search_result = self.qbt_client.search_results(id=self.search_job.id, limit = 3, offset = 1)
+        
+        self.magnet_list = [result.fileUrl for result in self.search_result.results] 
+
 #Bot functions
 def start(bot, update):
-        bot.send_message('Hi, this is a bot to search for torrents. Use /search <name>.')
+    bot.send_message('Hi, this is a bot to search for torrents. Use /search <name>.')
 
 def search(bot, update):
 
-        msg = update.message
-        chat_id = msg.chat_id
+    msg = update.message
+    chat_id = msg.chat_id
 
-        text = msg.text.split(' ')
-        search = text[1]
-        print(search)
+    text = msg.text.split(' ')
+    search = " "
+    search = search.join(text[1:])
+    print(search)
 
-        qbt_client = qbittorrentapi.Client(host='192.168.1.248:8080', username='admin', password='adminadmin')
+    bot.send_message(chat_id=chat_id, text="Fetching results...")
 
-        try:
-            qbt_client.auth_log_in()
-        except qbittorrentapi.LoginFailed as e:
-            print(e)
+    search_obj = qbt_search(search)
 
-        search_job = qbt_client.search.start(pattern=search, category='all', plugins='all')
+    title = ''
 
-        while (search_job.status()[0].status != 'Stopped'):
-            time.sleep(.1)
+    for result in search_obj.search_result.results:
+        title = title + f"Name: {result.fileName}\nSeeders: {result.nbSeeders}\nSize: {result.fileSize}\n\n"
 
-        search_result = qbt_client.search_results(id=search_job.id, limit = 3, offset = 1)
+    keyboard = [[InlineKeyboardButton(text="Magnet 1", callback_data=search+'0')],
+                [InlineKeyboardButton(text="Magnet 2", callback_data=search+'1')],
+                [InlineKeyboardButton(text="Magnet 3", callback_data=search+'2')]]
 
-        magnet_list = []
-        title = ''
-
-        for result in search_result.results:
-            i = result.fileUrl.index('&')
-            title = title + f"Name: {result.fileName}\nSeeders: {result.nbSeeders}\nSize: {result.fileSize}\n\n"
-            magnet_list.append(f"Magnet:\n{result.fileUrl[:i]}")
-            print(magnet_list)
-
-        keyboard = [[InlineKeyboardButton(text="Magnet 1", callback_data=magnet_list[0])],
-                    [InlineKeyboardButton(text="Magnet 2", callback_data=magnet_list[1])],
-                    [InlineKeyboardButton(text="Magnet 3", callback_data=magnet_list[2])]]
-
-        kb_markup = InlineKeyboardMarkup(keyboard)
+    kb_markup = InlineKeyboardMarkup(keyboard)
+    try:
         bot.send_message(chat_id=chat_id, text=title, reply_markup=kb_markup)
+    except: bot.send_message(chat_id=chat_id, text="No results found")
 
 def button(bot, update):
-        query = update.callback_query
+    
+    query = update.callback_query
+    query.answer()
+    
+    query.bot.send_message(chat_id=query.message.chat_id, text="sending magnet")
 
-        query.bot.send_message(chat_id=query.message.chat_id, text=query.data)
+    search_obj = qbt_search(query.data[:-1])
+    index = int(query.data[-1])
+    
+    query.bot.send_message(chat_id=query.message.chat_id, text=search_obj.magnet_list[index])
 
 def main():
 
-        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-        updater = Updater(token="1128514006:AAHVDTxMv_mWqnZzghzPWfGxHwiSVOKfgY8")
+    updater = Updater(token="")
 
-        dispatcher = updater.dispatcher
+    dispatcher = updater.dispatcher
 
-        search_handler = CommandHandler('search', search)
+    search_handler = CommandHandler('search', search)
+    dispatcher.add_handler(CallbackQueryHandler(button))
 
-        dispatcher.add_handler(search_handler)
+    dispatcher.add_handler(search_handler)
 
-        updater.start_polling()
-        updater.idle()
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
     main()
--- search.py
+++ search.py
@@ -1,23 +1,29 @@
 import qbittorrentapi
 import time
+import pprint
 
-qbt_client = qbittorrentapi.Client(host='192.168.1.248:8080', username='admin', password='adminadmin')
+pp = pprint.PrettyPrinter(indent=4)
+
+qbt_client = qbittorrentapi.Client(host='', username='', password='')
 
 try:
     qbt_client.auth_log_in()
 except qbittorrentapi.LoginFailed as e:
     print(e)
 
-search_job = qbt_client.search.start(pattern='lucifer', category='all', plugins='all')
+search = input("search for: ")
+limit = input("No. of torrents: ")
+
+search_job = qbt_client.search.start(pattern=search, category='all', plugins='all')
 
 while (search_job.status()[0].status != 'Stopped'):
     time.sleep(.1)
 
-search_result = qbt_client.search_results(id=search_job.id, limit = 3, offset = 1)
+search_result = qbt_client.search_results(id=search_job.id, limit = limit, offset = 1)
 
 for result in search_result.results:
-    i = result.fileUrl.index('&')
-    print(f"Name: {result.fileName}\nSeeders: {result.nbSeeders}\nSize: {result.fileSize}")
-    print(f"\nMagnet: {result.fileUrl[:i]}")
+    #print(f"Name: {result.fileName}\nSeeders: {result.nbSeeders}\nSize: {result.fileSize}")
+    #print(f"\nMagnet: {result.fileUrl}")
+    pp.pprint(result)
 
 search_job.delete()
